84567a92c89d090a4f4ec56b6bcbc6f58ab29352
Yurii Kulagin <yuk.178@bk.ru>
2022-08-04 19:06:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 119;
+        int oneMile = 20;
+        int miles = ticket / oneMile;
+        System.out.println("У вас " + miles + " миль(и)");
+
+
+    }
+}
